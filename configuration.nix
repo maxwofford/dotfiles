0f1a15bd9535f6ba9207e5bfd0dbe3f1e8bd70a0
Max Wofford <max@maxwofford.com>
2021-09-19 14:43:27 -0400

    Add watermelon through tailscale

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./tailscale.nix
+      ./watermelon.nix
     ];
 
   nixpkgs.config.allowUnfree = true;
---
+++ watermelon.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./tailscale.nix ];
+
+  security.wrappers."mount.nfs".source = "${pkgs.nfs-utils.out}/bin/mount.nfs";
+
+  fileSystems."/mnt/watermelon" = {
+    device = "//watermelon/data";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-5s";
+    in ["in${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+}
