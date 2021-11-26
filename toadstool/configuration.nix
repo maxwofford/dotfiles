ad57e0a34766212a974ff03b3dee37901c09f57d
Max Wofford <max@maxwofford.com>
2021-11-26 03:26:41 -0500

    Setup '.local' domains on toadstool

--- toadstool/configuration.nix
+++ toadstool/configuration.nix
@@ -18,6 +18,11 @@
     ];
   nixpkgs.config.allowUnfree = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
