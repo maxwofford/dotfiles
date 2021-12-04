53b90c32dd3f074b111e958084c7d3484f144bb4
Max Wofford <max@maxwofford.com>
2021-12-04 04:20:53 -0500

    Setup github copilot in vs-code config

--- common/home-manager/vs-code.nix
+++ common/home-manager/vs-code.nix
@@ -1,14 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+let
+  v21_11 = import
+    (builtins.fetchTarball "https://github.com/nixos/nixpkgs/tarball/21.11") {
+      config.allowUnfree = true;
+    };
+  inherit (vscode-utils) buildVscodeMarketplaceExtension;
+in {
   nixpkgs.config.allowUnfree = true;
+
   programs.vscode = {
     # This is enabled on a per-host basis
     # enable = true; # Did you read the comment?
     package = pkgs.vscode;
-    extensions = with pkgs.vscode-extensions; [
-      davidanson.vscode-markdownlint
-      bbenoist.Nix
-      eamodio.gitlens
-      vscodevim.vim
+    extensions = lib.mkMerge [
+      (with pkgs.vscode-extensions; [
+        davidanson.vscode-markdownlint
+        bbenoist.Nix
+        brettm12345.nixfmt-vscode
+        ms-azuretools.vscode-docker
+        eamodio.gitlens
+        vscodevim.vim
+      ])
+      (with v21_11.pkgs.vscode-extensions; [ github.copilot ])
     ];
   };
 }
