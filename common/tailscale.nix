288555423d901e53cce91f97de45603f6e22e686
Max Wofford <max@maxwofford.com>
2022-02-15 00:08:13 -0500

    Add tailscale IP list

--- common/tailscale.nix
+++ common/tailscale.nix
@@ -1,21 +1,18 @@
-{ config, ... }:
-
-{
+{ config, lib, ... }:
+let
+  authorizedIPs = import ./tailscale-ips.nix;
+  authorizedIPsList = lib.attrsets.mapAttrsToList (name: value: value + " " + name) authorizedIPs;
+in {
   services.tailscale.enable = true;
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.allowedUDPPorts = [
     config.services.tailscale.port
   ];
-  networking.extraHosts =
-    ''
-      100.115.193.119 forest
-      100.103.124.128 water-lily
-      100.64.232.97 angel-trumpet
-      100.113.78.4 guava
-      100.81.20.86 sequoia
-      100.96.50.76 watermelon
-      100.76.38.42 rubber
-      100.64.63.40 toadstool
-      100.118.73.5 sugarcane
-    '';
+  networking.extraHosts = ''
+
+  # Added by common/tailscale.nix
+  ${
+    builtins.concatStringsSep "\n" authorizedIPsList
+  }
+  '';
 }
