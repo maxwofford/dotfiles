e576a40e9052488c4892c6eb5183ee4a3c28cc1f
Max Wofford <max@maxwofford.com>
2022-02-14 23:37:19 -0500

    Version lock nix-vscode-server

--- common/home-manager/vscode-server.nix
+++ common/home-manager/vscode-server.nix
@@ -3,7 +3,7 @@
   imports = [
     "${
       fetchTarball
-      "https://github.com/msteen/nixos-vscode-server/tarball/master"
+      "https://github.com/msteen/nixos-vscode-server/tarball/bc28cc2a7d866b32a8358c6ad61bea68a618a3f5"
     }/modules/vscode-server/home.nix"
   ];
 }
