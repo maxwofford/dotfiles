e213cd12305025a871618a6a505a54aee0325480
Max Wofford <max@maxwofford.com>
2022-07-06 01:50:43 -0400

    Add pv & gtop to toadstool

--- toadstool/configuration.nix
+++ toadstool/configuration.nix
@@ -68,8 +68,10 @@
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    pv # monitor dd progress
     zoom-us
     htop
+    gtop
     wget
     git
     gh
