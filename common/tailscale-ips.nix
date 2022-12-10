092eddbaf8512fea26efb247d4cf7ef56decb3f2
Max Wofford <max@maxwofford.com>
2022-12-10 00:38:51 +0000

    Update tailscale IP list

--- common/tailscale-ips.nix
+++ common/tailscale-ips.nix
@@ -1,13 +1,15 @@
 {
-  forest        = "100.115.193.119";
-  water-lily    = "100.103.124.128";
-  angel-trumpet = "100.64.232.97";
-  guava         = "100.113.78.4";
-  sequoia       = "100.81.20.86";
-  watermelon    = "100.96.50.76";
-  rubber        = "100.76.38.42";
-  toadstool     = "100.64.63.40";
-  sugarcane     = "100.118.73.5";
-  agave         = "100.117.143.66";
-  bamboo        = "100.73.17.58";
+  forest               = "100.115.193.119";
+  water-lily           = "100.103.124.128";
+  angel-trumpet        = "100.64.232.97";
+  guava                = "100.113.78.4";
+  sequoia              = "100.81.20.86";
+  watermelon           = "100.96.50.76";
+  rubber               = "100.76.38.42";
+  toadstool            = "100.64.63.40";
+  sugarcane            = "100.118.73.5";
+  agave                = "100.117.143.66";
+  bamboo               = "100.97.36.51";
+  eucalyptus           = "100.124.72.53";
+  golden-barrel-cactus = "100.79.48.89";
 }
