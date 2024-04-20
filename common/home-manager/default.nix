4128f54d21378972cc91409e920b8973e7c06934
Max Wofford <max@maxwofford.com>
2024-04-20 03:02:35 +0000

    Forest: Upgrade state version to 23.11

--- common/home-manager/default.nix
+++ common/home-manager/default.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }:
 let
   home-manager = builtins.fetchTarball
-    "https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz";
+    "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
 in {
   imports = [ (import "${home-manager}/nixos") ];
 
   home-manager.users.msw = { pkgs, config, ... }: {
+    home.stateVersion = "23.11";
     imports = [
       ./bash.nix
       ./brave.nix
